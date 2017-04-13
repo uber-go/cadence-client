e33bab5b59ff3d68e5f232c0b04dd10112bdb306
Yimin Chen <yiminc@uber.com>
2017-04-12 17:07:45 -0700

    use reflection to print data

--- common/util/stringer.go
+++ common/util/stringer.go
@@ -1,150 +1,161 @@
 package util
 
 import (
+	"bytes"
 	"fmt"
+	"reflect"
 
 	s "github.com/uber-go/cadence-client/.gen/go/shared"
 )
 
-// TODO: find a better way to present input/result []byte
+func anyToString(d interface{}) string {
+	var buf bytes.Buffer
+	v := reflect.ValueOf(d)
+	t := reflect.TypeOf(d)
+	switch v.Kind() {
+	case reflect.Ptr:
+		buf.WriteString(anyToString(v.Elem().Interface()))
+	case reflect.Struct:
+		buf.WriteString("(")
+		for i := 0; i < v.NumField(); i++ {
+			f := v.Field(i)
+			if f.Kind() == reflect.Invalid {
+				continue
+			}
+			fieldValue := valueToString(f)
+			if len(fieldValue) == 0 {
+				continue
+			}
+			if i > 0 {
+				buf.WriteString(", ")
+			}
+			buf.WriteString(fmt.Sprintf("%s:%s", t.Field(i).Name, fieldValue))
+		}
+		buf.WriteString(")")
+	default:
+		buf.WriteString(fmt.Sprint(d))
+	}
+	return buf.String()
+}
+
+func valueToString(v reflect.Value) string {
+	switch v.Kind() {
+	case reflect.Ptr:
+		return valueToString(v.Elem())
+	case reflect.Struct:
+		return anyToString(v.Interface())
+	case reflect.Invalid:
+		return ""
+	case reflect.Slice:
+		// TODO: find a better way to handle this.
+		return fmt.Sprintf("len=%d", v.Len())
+	default:
+		return fmt.Sprint(v.Interface())
+	}
+}
 
 // HistoryEventToString convert HistoryEvent to string
 func HistoryEventToString(e *s.HistoryEvent) string {
-
+	var attr interface{}
 	switch e.GetEventType() {
-
 	case s.EventType_WorkflowExecutionStarted:
-		attr := e.GetWorkflowExecutionStartedEventAttributes()
-		return fmt.Sprintf("%s, Identity: %s, WorkflowType: %s, TaskList: %s, InputSize: %d",
-			e.GetEventType(), attr.GetIdentity(), attr.GetWorkflowType().GetName(), attr.GetTaskList().GetName(), len(attr.GetInput()))
+		attr = e.GetWorkflowExecutionStartedEventAttributes()
 
 	case s.EventType_WorkflowExecutionCompleted:
-		attr := e.GetWorkflowExecutionCompletedEventAttributes()
-		return fmt.Sprintf("%s, ResultSize: %d", e.GetEventType(), len(attr.GetResult_()))
+		attr = e.GetWorkflowExecutionCompletedEventAttributes()
 
 	case s.EventType_WorkflowExecutionFailed:
-		attr := e.GetWorkflowExecutionFailedEventAttributes()
-		return fmt.Sprintf("%s, Reason: %d", e.GetEventType(), attr.GetReason())
+		attr = e.GetWorkflowExecutionFailedEventAttributes()
 
 	case s.EventType_WorkflowExecutionTimedOut:
-		attr := e.GetWorkflowExecutionTimedOutEventAttributes()
-		return fmt.Sprintf("%s, TimeoutType: %s", e.GetEventType(), attr.GetTimeoutType())
+		attr = e.GetWorkflowExecutionTimedOutEventAttributes()
 
 	case s.EventType_DecisionTaskScheduled:
-		attr := e.GetDecisionTaskScheduledEventAttributes()
-		return fmt.Sprintf("%s, TaskList: %s, StartToCloseTimeoutSeconds: %d", e.GetEventType(), attr.GetTaskList().GetName(), attr.GetStartToCloseTimeoutSeconds())
+		attr = e.GetDecisionTaskScheduledEventAttributes()
 
 	case s.EventType_DecisionTaskStarted:
-		attr := e.GetDecisionTaskStartedEventAttributes()
-		return fmt.Sprintf("%s, Identity: %s, RequestId: %s, ScheduledEventId: %d", e.GetEventType(), attr.GetIdentity(), attr.GetRequestId(), attr.GetScheduledEventId())
+		attr = e.GetDecisionTaskStartedEventAttributes()
 
 	case s.EventType_DecisionTaskCompleted:
-		attr := e.GetDecisionTaskCompletedEventAttributes()
-		return fmt.Sprintf("%s, Identity: %s, ScheduledEventId: %d, StartedEventId: %d", e.GetEventType(), attr.GetIdentity(), attr.GetScheduledEventId(), attr.GetStartedEventId())
+		attr = e.GetDecisionTaskCompletedEventAttributes()
 
 	case s.EventType_DecisionTaskTimedOut:
-		attr := e.GetDecisionTaskTimedOutEventAttributes()
-		return fmt.Sprintf("%s, TimeoutType: %s, ScheduledEventId: %d, StartedEventId: %d", e.GetEventType(), attr.GetTimeoutType(), attr.GetScheduledEventId(), attr.GetStartedEventId())
+		attr = e.GetDecisionTaskTimedOutEventAttributes()
 
 	case s.EventType_ActivityTaskScheduled:
-		attr := e.GetActivityTaskScheduledEventAttributes()
-		return fmt.Sprintf("%s, ActivityType: %s, ActivityId: %s, TaskList: %s, InputSize: %d",
-			e.GetEventType(), attr.GetActivityType().GetName(), attr.GetActivityId(), attr.GetTaskList().GetName(), len(attr.GetInput()))
+		attr = e.GetActivityTaskScheduledEventAttributes()
 
 	case s.EventType_ActivityTaskStarted:
-		attr := e.GetActivityTaskStartedEventAttributes()
-		return fmt.Sprintf("%s, Identity: %s, RequestId: %s, ScheduledEventId: %d", e.GetEventType(), attr.GetIdentity(), attr.GetRequestId(), attr.GetScheduledEventId())
+		attr = e.GetActivityTaskStartedEventAttributes()
 
 	case s.EventType_ActivityTaskCompleted:
-		attr := e.GetActivityTaskCompletedEventAttributes()
-		return fmt.Sprintf("%s, Identity: %s, ScheduledEventId: %d, StartedEventId: %d, ResultSize: %d", e.GetEventType(), attr.GetIdentity(), attr.GetStartedEventId(), attr.GetScheduledEventId(), len(attr.GetResult_()))
+		attr = e.GetActivityTaskCompletedEventAttributes()
 
 	case s.EventType_ActivityTaskFailed:
-		attr := e.GetActivityTaskFailedEventAttributes()
-		return fmt.Sprintf("%s, Identity: %s, ScheduledEventId: %d, StartedEventId: %d, Reason: %s", e.GetEventType(), attr.GetIdentity(), attr.GetStartedEventId(), attr.GetScheduledEventId(), attr.GetReason())
+		attr = e.GetActivityTaskFailedEventAttributes()
 
 	case s.EventType_ActivityTaskTimedOut:
-		attr := e.GetActivityTaskTimedOutEventAttributes()
-		return fmt.Sprintf("%s, TimeoutType: %s, ScheduledEventId: %d, StartedEventId: %d", e.GetEventType(), attr.GetTimeoutType(), attr.GetStartedEventId(), attr.GetScheduledEventId())
+		attr = e.GetActivityTaskTimedOutEventAttributes()
 
 	case s.EventType_ActivityTaskCancelRequested:
-		attr := e.GetActivityTaskCancelRequestedEventAttributes()
-		return fmt.Sprintf("%s, ActivityId: %s", e.GetEventType(), attr.GetActivityId())
+		attr = e.GetActivityTaskCancelRequestedEventAttributes()
 
 	case s.EventType_RequestCancelActivityTaskFailed:
-		attr := e.GetRequestCancelActivityTaskFailedEventAttributes()
-		return fmt.Sprintf("%s, ActivityId: %s, Cause: %s", e.GetEventType(), attr.GetActivityId(), attr.GetCause())
+		attr = e.GetRequestCancelActivityTaskFailedEventAttributes()
 
 	case s.EventType_ActivityTaskCanceled:
-		attr := e.GetActivityTaskCanceledEventAttributes()
-		return fmt.Sprintf("%s, Identity: %s, ScheduledEventId: %d, LatestCancelRequestedEventId %d", e.GetEventType(), attr.GetIdentity(), attr.GetScheduledEventId(), attr.GetLatestCancelRequestedEventId())
+		attr = e.GetActivityTaskCanceledEventAttributes()
 
 	case s.EventType_TimerStarted:
-		attr := e.GetTimerStartedEventAttributes()
-		return fmt.Sprintf("%s, TimerId: %s, StartToFireTimeoutSeconds: %d",
-			e.GetEventType(), attr.GetTimerId(), attr.GetStartToFireTimeoutSeconds())
+		attr = e.GetTimerStartedEventAttributes()
 
 	case s.EventType_TimerFired:
-		attr := e.GetTimerFiredEventAttributes()
-		return fmt.Sprintf("%s, TimerId: %s", e.GetEventType(), attr.GetTimerId())
+		attr = e.GetTimerFiredEventAttributes()
 
 	case s.EventType_CompleteWorkflowExecutionFailed:
-		attr := e.GetCompleteWorkflowExecutionFailedEventAttributes()
-		return fmt.Sprintf("%s, Cause: %s", e.GetEventType(), attr.GetCause())
+		attr = e.GetCompleteWorkflowExecutionFailedEventAttributes()
 
 	case s.EventType_CancelTimerFailed:
-		attr := e.GetCancelTimerFailedEventAttributes()
-		return fmt.Sprintf("%s, Identity: %s, TimerId: %s, Cause: %s", e.GetEventType(), attr.GetIdentity(), attr.GetTimerId(), attr.GetCause())
+		attr = e.GetCancelTimerFailedEventAttributes()
 
 	case s.EventType_TimerCanceled:
-		attr := e.GetTimerCanceledEventAttributes()
-		return fmt.Sprintf("%s, TimerId: %s", e.GetEventType(), attr.GetTimerId())
+		attr = e.GetTimerCanceledEventAttributes()
 
 	case s.EventType_MarkerRecorded:
-		attr := e.GetMarkerRecordedEventAttributes()
-		return fmt.Sprintf("%s, Marker: %s", e.GetEventType(), attr.GetMarkerName())
+		attr = e.GetMarkerRecordedEventAttributes()
 
 	case s.EventType_WorkflowExecutionTerminated:
-		attr := e.GetWorkflowExecutionTerminatedEventAttributes()
-		return fmt.Sprintf("%s, Identity :%s, Reason: %s", e.GetEventType(), attr.GetIdentity(), attr.GetReason())
+		attr = e.GetWorkflowExecutionTerminatedEventAttributes()
 	}
 
-	return e.GetEventType().String()
+	return e.GetEventType().String() + ": " + anyToString(attr)
 }
 
 // DecisionToString convert Decision to string
 func DecisionToString(d *s.Decision) string {
+	var attr interface{}
 	switch d.GetDecisionType() {
 	case s.DecisionType_ScheduleActivityTask:
-		attr := d.GetScheduleActivityTaskDecisionAttributes()
-		return fmt.Sprintf("%s, ActivityType: %s, ActivityId: %s, TaskList: %s, InputSize: %d",
-			d.GetDecisionType(), attr.GetActivityType().GetName(), attr.GetActivityId(), attr.GetTaskList().GetName(), len(attr.GetInput()))
+		attr = d.GetScheduleActivityTaskDecisionAttributes()
 
 	case s.DecisionType_RequestCancelActivityTask:
-		attr := d.GetRequestCancelActivityTaskDecisionAttributes()
-		return fmt.Sprintf("%s, ActivityId: %s", d.GetDecisionType(), attr.GetActivityId())
+		attr = d.GetRequestCancelActivityTaskDecisionAttributes()
 
 	case s.DecisionType_StartTimer:
-		attr := d.GetStartTimerDecisionAttributes()
-		return fmt.Sprintf("%s, TimerId: %s, StartToFireTimeoutSeconds: %d",
-			d.GetDecisionType(), attr.GetTimerId(), attr.GetStartToFireTimeoutSeconds())
+		attr = d.GetStartTimerDecisionAttributes()
 
 	case s.DecisionType_CancelTimer:
-		attr := d.GetCancelTimerDecisionAttributes()
-		return fmt.Sprintf("%s, TimerId: %s", d.GetDecisionType(), attr.GetTimerId())
+		attr = d.GetCancelTimerDecisionAttributes()
 
 	case s.DecisionType_CompleteWorkflowExecution:
-		attr := d.GetCompleteWorkflowExecutionDecisionAttributes()
-		return fmt.Sprintf("%s, ResultSize: %d", d.GetDecisionType(), len(attr.GetResult_()))
+		attr = d.GetCompleteWorkflowExecutionDecisionAttributes()
 
 	case s.DecisionType_FailWorkflowExecution:
-		attr := d.GetFailWorkflowExecutionDecisionAttributes()
-		return fmt.Sprintf("%s, Reason: %d", d.GetDecisionType(), attr.GetReason())
+		attr = d.GetFailWorkflowExecutionDecisionAttributes()
 
 	case s.DecisionType_RecordMarker:
-		attr := d.GetRecordMarkerDecisionAttributes()
-		return fmt.Sprintf("%s, MarkerName: %d", d.GetDecisionType(), attr.GetMarkerName())
+		attr = d.GetRecordMarkerDecisionAttributes()
 	}
 
-	return d.GetDecisionType().String()
+	return d.GetDecisionType().String() + ": " + anyToString(attr)
 }

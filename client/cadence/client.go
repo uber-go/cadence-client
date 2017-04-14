dd18807a29b553d181c5e250c1f898ffea6d8520
Yimin Chen <yiminc@uber.com>
2017-04-14 12:57:58 -0700

    add visibility api to client

--- client/cadence/client.go
+++ client/cadence/client.go
@@ -37,6 +37,12 @@ type (
 
 		// RecordActivityHeartbeat records heartbeat for an activity.
 		RecordActivityHeartbeat(taskToken, details []byte) error
+
+		// ListClosedWorkflow gets closed workflow executions based on request filters
+		ListClosedWorkflow(request *s.ListClosedWorkflowExecutionsRequest) (*s.ListClosedWorkflowExecutionsResponse, error)
+
+		// ListClosedWorkflow gets open workflow executions based on request filters
+		ListOpenWorkflow(request *s.ListOpenWorkflowExecutionsRequest) (*s.ListOpenWorkflowExecutionsResponse, error)
 	}
 
 	// ClientOptions are optional parameters for Client creation.

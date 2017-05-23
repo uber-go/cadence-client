f87994aa6710c4dcc6679452cad71ad9bff9ae43
Maxim Fateev <maxim@uber.com>
2017-05-23 16:44:33 -0700

    Moved EncodedValue to workflow.go

--- internal_workflow_testsuite.go
+++ internal_workflow_testsuite.go
@@ -843,5 +843,9 @@ func (env *testWorkflowEnvironmentImpl) getActivityInfo(activityID, activityType
 	}
 }
 
+func (env *testWorkflowEnvironmentImpl) SideEffect(f func() []byte, callback resultHandler) {
+	panic("not supported yet")
+}
+
 // make sure interface is implemented
 var _ workflowEnvironment = (*testWorkflowEnvironmentImpl)(nil)

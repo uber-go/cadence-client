8e8bebae3c65d68790b861f4eb682145c159e607
Maxim Fateev <maxim@uber.com>
2017-05-30 16:21:13 -0700

    Added Test_IsVersion unit test

--- internal_workflow_testsuite_test.go
+++ internal_workflow_testsuite_test.go
@@ -440,28 +440,31 @@ func (s *WorkflowTestSuiteUnitTest) Test_SideEffect() {
 	s.Nil(env.GetWorkflowError())
 }
 
-const Component = "workflow"
+const testComponent = "workflow"
 const (
-	Version1 = iota + 10
-	Version2
-	Version3
+	testVersion1 = iota + 10
+	testVersion2
+	testVersion3
 )
 
-const CurrentVersion = Version3
+const testCurrentVersion = testVersion3
+
+type keyType string
+
+var nameKey keyType = "name"
+
+func setName(ctx context.Context, name string) {
+	value := ctx.Value(nameKey).(*string)
+	*value = name
+}
 
 func oldActivity(ctx context.Context, msg string) (string, error) {
+	setName(ctx, "oldActivity")
 	return "hello" + "_" + msg, nil
 }
 
 func newActivity(ctx context.Context, msg string) (string, error) {
-	return "hello" + "_" + msg, nil
-}
-
-func newActivity2(ctx context.Context, msg string) (string, error) {
-	return "hello" + "_" + msg, nil
-}
-
-func newActivity3(ctx context.Context, msg string) (string, error) {
+	setName(ctx, "newActivity")
 	return "hello" + "_" + msg, nil
 }
 
@@ -469,59 +472,25 @@ func (s *WorkflowTestSuiteUnitTest) Test_IsVersion() {
 	workflowFn := func(ctx Context) error {
 		ctx = WithActivityOptions(ctx, s.activityOptions)
 		var f Future
-
-		f = ExecuteActivity(ctx, oldActivity, "msg1")
-
-		f = ExecuteActivity(ctx, newActivity, "anotherMsg")
-
-		v := GetVersion(ctx, Component, CurrentVersion)
+		v := GetVersion(ctx, testComponent, testCurrentVersion, DefaultVersion)
 		if v == DefaultVersion {
 			f = ExecuteActivity(ctx, oldActivity, "msg1")
 		} else {
 			f = ExecuteActivity(ctx, newActivity, "anotherMsg")
 		}
-
 		err := f.Get(ctx, nil) // wait for result
 		return err
 	}
 
 	env := s.NewTestWorkflowEnvironment()
-
+	RegisterActivity(oldActivity)
+	RegisterActivity(newActivity)
+	var name string
+	ctx := context.WithValue(context.Background(), nameKey, &name)
+	env.SetWorkerOption(WorkerOptions{BackgroundActivityContext: ctx})
 	env.ExecuteWorkflow(workflowFn)
 
 	s.True(env.IsWorkflowCompleted())
 	s.Nil(env.GetWorkflowError())
-}
-
-func (s *WorkflowTestSuiteUnitTest) Test_IsVersion2() {
-	workflowFn := func(ctx Context) error {
-		ctx = WithActivityOptions(ctx, s.activityOptions)
-		var f Future
-
-		f = ExecuteActivity(ctx, oldActivity, "msg1")
-
-		f = ExecuteActivity(ctx, newActivity, "anotherMsg")
-
-		v := GetVersion(ctx, Component, CurrentVersion)
-		switch v {
-		case DefaultVersion:
-			f = ExecuteActivity(ctx, oldActivity, "msg1")
-		case Version1:
-			f = ExecuteActivity(ctx, newActivity, "anotherMsg")
-		case Version2:
-			f = ExecuteActivity(ctx, newActivity2, "anotherMsg2")
-		default: // Default to support larger versions without code change
-			f = ExecuteActivity(ctx, newActivity3, "anotherMsg3")
-		}
-
-		err := f.Get(ctx, nil) // wait for result
-		return err
-	}
-
-	env := s.NewTestWorkflowEnvironment()
-
-	env.ExecuteWorkflow(workflowFn)
-
-	s.True(env.IsWorkflowCompleted())
-	s.Nil(env.GetWorkflowError())
+	s.EqualValues("newActivity", name)
 }

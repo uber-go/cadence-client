b252fafca39150e484c05650282afcfb728ee5dc
Steven Littiebrant <imgroxx@gmail.com>
2018-04-03 16:04:45 -0700

    Clean up / optimize makefile, sandbox builds better.
    - removed a bunch of cruft from overly-generic makefile copypasta
      (many of it doesn't do anything here, and makes it harder to read).
    - corrected a number of dependency chains.  let me know if I missed
      something!  I've tried to be thorough, but it's hard to be sure.
      in any case, now it shouldn't re-do work unless it's necessary, and
      it can even init in parallel (`make -j4`) from a fresh clone without
      any problems I've seen.
    - I was unable to trick glide into including the necessary packages to build
      things out of vendor (dep appears to work with this, if added to "require"),
      so dependencies like thriftrw/yarpc/golint are now cloned, checked out, and
      built separately so they can be locked down to a version.  see the new
      versioned_go_build.sh file for details.  to change versions, just change the
      version arg, rm the binary (or `make clean`) and rebuild.
    - last but not least: `go test ./...` is equivalent to (or better than)
      testing each package separately, and is often *much* faster because it
      doesn't spend time rebuilding everything.

---
+++ versioned_go_build.sh
@@ -0,0 +1,67 @@
+#!/bin/bash
+
+set -e
+
+usage () {
+    echo 'Installs a specific version of a go-gettable bin to the specified location.'
+    echo ''
+    echo 'Usage:'
+    echo ''
+    echo -e "\t$0 [--debug] go-gettable-repo version [path-to-bin-in-repo] install-location"
+    echo ''
+    echo 'Examples:'
+    echo ''
+    echo -e "\t$0 go.uber.org/thriftrw 1.10.0 somewhere/thriftrw"
+    echo -e '\t\Installs v1.10.0 of go.uber.org/thriftrw to somewhere/thriftrw.'
+    echo -e '\t\tNotice that go.uber.org/thriftrw is both the repository AND the binary name.'
+    echo ''
+    echo -e "\t$0 golang.org/x/lint SOME_SHA golint somewhere/golint"
+    echo -e '\t\Installs a specific SHA of e.g. "golang.org/x/lint/golint" to "somewhere/golint",'
+    echo -e '\t\tNotice that the golint bin is in a subfolder of the golang.org/x/lint repository.'
+
+    exit 1
+}
+
+# needs 3 or 4 args, plus optional --debug
+
+if [ "$1" == "--debug" ]; then
+    shift  # consume it
+    DEBUG=1
+fi
+
+[ $# -ge 3 ] || usage
+[ $# -le 4 ] || usage
+
+[ -z $DEBUG ] || set -x
+
+# set up gopath, and make sure it's cleaned up regardless of how we exit
+export GOPATH=$(mktemp -d)
+trap "rm -rf $GOPATH" EXIT
+
+# variable inits
+GO_GETTABLE_REPO="$1"
+VERSION="$2"
+if [ $# -eq 4 ]; then
+    GO_GETTABLE_BIN="$1/$3"
+    INSTALL_LOCATION="$4"
+elif [ $# -eq 3 ]; then
+    GO_GETTABLE_BIN="$1"
+    INSTALL_LOCATION="$3"
+else
+    # should be unreachable
+    help
+fi
+
+
+go get -d "$GO_GETTABLE_BIN"
+pushd "$GOPATH/src/$GO_GETTABLE_REPO"
+git checkout "$VERSION"
+
+# only glide install when there is a glide file, or it tries to install
+# to the current repo (not in our current folder)
+if [ -f glide.lock ]; then
+    glide install
+fi
+
+popd
+go build -o "$INSTALL_LOCATION" "$GO_GETTABLE_BIN"

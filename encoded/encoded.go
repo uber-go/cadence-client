ab6afd01a4919aff6d1095cd708292fa937e2c06
Bowei Xu <boweixu@uber.com>
2018-05-23 23:35:16 -0700

    Add comments on DataConverter interface

--- encoded/encoded.go
+++ encoded/encoded.go
@@ -40,6 +40,15 @@ type (
 	}
 
 	// DataConverter is used by the framework to serialize/deserialize method parameters that need to be sent over the wire.
+	//
+	// To encode/decode workflow arguments, one should set DataConverter in two places:
+	//   1. Workflow worker, through worker.Options
+	//   2. Client, through client.Options
+	// To encode/decode Activity/ChildWorkflow arguments, one should set DataConverter two 2 places:
+	//   1. Inside workflow code, use workflow.WithDataConverter to create new Context,
+	// and pass that context ExecuteActivity/ExecuteChildWorkflow calls.
+	// Cadence support using different DataConverters for different activity/childWorkflow in same workflow.
+	//   2. Activity/Workflow worker that run these activity/childWorkflow, through worker.Options.
 	DataConverter interface {
 		// ToData implements conversion of a list of values.
 		ToData(value ...interface{}) ([]byte, error)

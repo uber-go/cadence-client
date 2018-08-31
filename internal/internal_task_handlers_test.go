1be9d7abfdf6c3064dfbea54ef3d0ab247f262b7
Samar Abbas <samar@uber.com>
2018-08-31 09:33:33 -0700

    Validate cancel handler is invoked

--- internal/internal_task_handlers_test.go
+++ internal/internal_task_handlers_test.go
@@ -577,17 +577,21 @@ func (t *TaskHandlersTestSuite) TestHeartBeat_NilResponseWithDomainNotActiveErro
 	domainNotActiveError := &s.DomainNotActiveError{}
 	mockService.EXPECT().RecordActivityTaskHeartbeat(gomock.Any(), gomock.Any(), callOptions...).Return(nil, domainNotActiveError)
 
+	called := false
+	cancelHandler := func () { called = true }
+
 	cadenceInvoker := newServiceInvoker(
 		nil,
 		"Test_Cadence_Invoker",
 		mockService,
-		func() {},
+		cancelHandler,
 		0)
 
 	heartbeatErr := cadenceInvoker.Heartbeat(nil)
 	t.NotNil(heartbeatErr)
 	_, ok := (heartbeatErr).(*s.DomainNotActiveError)
 	t.True(ok, "heartbeatErr must be DomainNotActiveError.")
+	t.True(called)
 }
 
 type testActivityDeadline struct {

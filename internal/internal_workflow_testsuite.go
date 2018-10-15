c6294aac8a1d2a1c0ea5b7d9563294a0d476618f
Andrew Dawson <dandrew@uber.com>
2018-10-15 10:25:30 -0700

    include workflowtype in testing code

--- internal/internal_workflow_testsuite.go
+++ internal/internal_workflow_testsuite.go
@@ -44,10 +44,11 @@ import (
 )
 
 const (
-	defaultTestDomain     = "default-test-domain"
-	defaultTestTaskList   = "default-test-tasklist"
-	defaultTestWorkflowID = "default-test-workflow-id"
-	defaultTestRunID      = "default-test-run-id"
+	defaultTestDomain           = "default-test-domain"
+	defaultTestTaskList         = "default-test-tasklist"
+	defaultTestWorkflowID       = "default-test-workflow-id"
+	defaultTestRunID            = "default-test-run-id"
+	defaultTestWorkflowTypeName = "default-test-workflow-type-name"
 )
 
 type (
@@ -439,6 +440,7 @@ func (env *testWorkflowEnvironmentImpl) executeActivity(
 		defaultTestWorkflowID,
 		defaultTestRunID,
 		"0",
+		defaultTestWorkflowTypeName,
 		params,
 	)
 
@@ -803,6 +805,7 @@ func (env *testWorkflowEnvironmentImpl) ExecuteActivity(parameters executeActivi
 		defaultTestWorkflowID,
 		defaultTestRunID,
 		activityInfo.activityID,
+		defaultTestWorkflowTypeName,
 		parameters,
 	)
 
@@ -1365,7 +1368,7 @@ func (env *testWorkflowEnvironmentImpl) newTestActivityTaskHandler(taskList stri
 	return taskHandler
 }
 
-func newTestActivityTask(workflowID, runID, activityID string, params executeActivityParams) *shared.PollForActivityTaskResponse {
+func newTestActivityTask(workflowID, runID, activityID, workflowTypeName string, params executeActivityParams) *shared.PollForActivityTaskResponse {
 	task := &shared.PollForActivityTaskResponse{
 		WorkflowExecution: &shared.WorkflowExecution{
 			WorkflowId: common.StringPtr(workflowID),
@@ -1380,6 +1383,9 @@ func newTestActivityTask(workflowID, runID, activityID string, params executeAct
 		StartedTimestamp:              common.Int64Ptr(time.Now().UnixNano()),
 		StartToCloseTimeoutSeconds:    common.Int32Ptr(params.StartToCloseTimeoutSeconds),
 		HeartbeatTimeoutSeconds:       common.Int32Ptr(params.HeartbeatTimeoutSeconds),
+		WorkflowType: &shared.WorkflowType{
+			Name: common.StringPtr(workflowTypeName),
+		},
 	}
 	return task
 }

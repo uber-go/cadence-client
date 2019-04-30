8b24fba8b1f1a009a655e600c8dadb51f6ca8d6b
Shreyas Srivatsan <shreyass@uber.com>
2019-04-30 16:23:45 -0700

    add test

--- internal/headers_test.go
+++ internal/headers_test.go
@@ -20,16 +20,148 @@
 
 package internal
 
-import "testing"
+import (
+	"context"
+	"testing"
+
+	"github.com/opentracing/opentracing-go"
+	"github.com/stretchr/testify/assert"
+	"go.uber.org/cadence/.gen/go/shared"
+)
 
 func TestHeaderWriter(t *testing.T) {
+	tests := []struct {
+		name     string
+		initial  *shared.Header
+		expected *shared.Header
+		vals     map[string][]byte
+	}{
+		{
+			"no values",
+			&shared.Header{
+				Fields: map[string][]byte{},
+			},
+			&shared.Header{
+				Fields: map[string][]byte{},
+			},
+			map[string][]byte{},
+		},
+		{
+			"add values",
+			&shared.Header{
+				Fields: map[string][]byte{},
+			},
+			&shared.Header{
+				Fields: map[string][]byte{
+					"key1": []byte("val1"),
+					"key2": []byte("val2"),
+				},
+			},
+			map[string][]byte{
+				"key1": []byte("val1"),
+				"key2": []byte("val2"),
+			},
+		},
+		{
+			"overwrite values",
+			&shared.Header{
+				Fields: map[string][]byte{
+					"key1": []byte("unexpected"),
+				},
+			},
+			&shared.Header{
+				Fields: map[string][]byte{
+					"key1": []byte("val1"),
+					"key2": []byte("val2"),
+				},
+			},
+			map[string][]byte{
+				"key1": []byte("val1"),
+				"key2": []byte("val2"),
+			},
+		},
+	}
 
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			writer := NewHeaderWriter(test.initial)
+			for key, val := range test.vals {
+				writer.Set(key, val)
+			}
+			assert.Equal(t, test.expected, test.initial)
+		})
+	}
 }
 
 func TestHeaderReader(t *testing.T) {
+	tests := []struct {
+		name    string
+		header  *shared.Header
+		keys    map[string]struct{}
+		isError bool
+	}{
+		{
+			"valid values",
+			&shared.Header{
+				Fields: map[string][]byte{
+					"key1": []byte("val1"),
+					"key2": []byte("val2"),
+				},
+			},
+			map[string]struct{}{"key1": struct{}{}, "key2": struct{}{}},
+			false,
+		},
+		{
+			"invalid values",
+			&shared.Header{
+				Fields: map[string][]byte{
+					"key1": []byte("val1"),
+					"key2": []byte("val2"),
+				},
+			},
+			map[string]struct{}{"key2": struct{}{}},
+			true,
+		},
+	}
 
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			reader := NewHeaderReader(test.header)
+			err := reader.ForEachKey(func(key string, val []byte) error {
+				if _, ok := test.keys[key]; !ok {
+					return assert.AnError
+				}
+				return nil
+			})
+			if test.isError {
+				assert.Error(t, err)
+			} else {
+				assert.NoError(t, err)
+			}
+		})
+	}
 }
 
 func TestTracingContextPropagator(t *testing.T) {
+	ctxProp := NewTracingContextPropagator(nil)
 
+	tracer := opentracing.NoopTracer{}
+	span := tracer.StartSpan("test-operation")
+	ctx := context.Background()
+	ctx = opentracing.ContextWithSpan(ctx, span)
+	header := &shared.Header{
+		Fields: map[string][]byte{},
+	}
+
+	err := ctxProp.Inject(ctx, NewHeaderWriter(header))
+	assert.NoError(t, err)
+
+	_, ok := header.Fields[tracingKey]
+	assert.True(t, ok)
+
+	returnCtx := context.Background()
+	returnCtx, err = ctxProp.Extract(returnCtx, NewHeaderReader(header))
+	assert.NoError(t, err)
+	newSpan := opentracing.SpanFromContext(returnCtx)
+	assert.Equal(t, span, newSpan)
 }

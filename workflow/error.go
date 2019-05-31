97ad57172c983a3957b546a6ad516f4f142cce6c
Yichao Yang <ycyang@uber.com>
2019-05-31 10:25:28 -0700

    bug fix

--- workflow/error.go
+++ workflow/error.go
@@ -126,7 +126,7 @@ func NewContinueAsNewError(ctx Context, wfn interface{}, args ...interface{}) *C
 // WARNING: This function is public only to support unit testing of workflows.
 // It shouldn't be used by application level code.
 func NewTimeoutError(timeoutType shared.TimeoutType, details ...interface{}) *TimeoutError {
-	return internal.NewTimeoutError(timeoutType, details)
+	return internal.NewTimeoutError(timeoutType, details...)
 }
 
 // NewHeartbeatTimeoutError creates TimeoutError instance

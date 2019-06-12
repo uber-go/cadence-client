0eecdf8bf5e2ad12fcda23000499a1672b29a484
Venkatraghavan Srinivasan <venkat@uber.com>
2019-06-12 14:50:25 -0700

    add debug info on gocov.sh

--- .buildkite/scripts/gocov.sh
+++ .buildkite/scripts/gocov.sh
@@ -11,6 +11,10 @@ buildkite-agent artifact download ".build/coverage/unit_test_cover.out" ".build/
 buildkite-agent artifact download ".build/coverage/integ_test_sticky_off_cover.out" ".build/coverage" --step "integration-test-sticky-off" --build "$BUILDKITE_BUILD_ID"
 buildkite-agent artifact download ".build/coverage/integ_test_sticky_on_cover.out" ".build/coverage" --step "integration-test-sticky-on" --build "$BUILDKITE_BUILD_ID"
 
+echo "download complete"
+
+ls -l .build/coverage
+
 # report coverage
 make cover_ci
 

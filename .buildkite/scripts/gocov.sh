e4591c0837a57038ab3831cd90146d936fc3cdf9
Venkatraghavan Srinivasan <venkat@uber.com>
2019-06-12 13:16:40 -0700

    add --build arg to download

--- .buildkite/scripts/gocov.sh
+++ .buildkite/scripts/gocov.sh
@@ -7,9 +7,9 @@ go get github.com/mattn/goveralls
 
 # download cover files from all the tests
 mkdir -p .build/coverage
-buildkite-agent artifact download .build/coverage/unit_test_cover.out .build/coverage --step unit-test
-buildkite-agent artifact download .build/coverage/integ_test_sticky_off_cover.out .build/coverage --step integration-test-sticky-off
-buildkite-agent artifact download .build/coverage/integ_test_sticky_on_cover.out .build/coverage --step integration-test-sticky-on
+buildkite-agent artifact download .build/coverage/unit_test_cover.out .build/coverage --step unit-test --build $BUILDKITE_BUILD_ID
+buildkite-agent artifact download .build/coverage/integ_test_sticky_off_cover.out .build/coverage --step integration-test-sticky-off --build $BUILDKITE_BUILD_ID
+buildkite-agent artifact download .build/coverage/integ_test_sticky_on_cover.out .build/coverage --step integration-test-sticky-on --build $BUILDKITE_BUILD_ID
 
 # report coverage
 make cover_ci

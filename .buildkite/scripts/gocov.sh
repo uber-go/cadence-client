c9179cb53d37fd5539fee24ce10f044ee25e484f
Venkatraghavan Srinivasan <venkat@uber.com>
2019-06-13 10:58:01 -0700

    cleanup

--- .buildkite/scripts/gocov.sh
+++ .buildkite/scripts/gocov.sh
@@ -13,12 +13,6 @@ buildkite-agent artifact download ".build/coverage/integ_test_sticky_on_cover.ou
 
 echo "download complete"
 
-CURRDIR=`pwd`
-echo $CURRDIR
-ls -al .build
-ls -al .build/coverage
-
-#export BUILDKITE_REPO="https://github.com/uber-go/cadence-client"
 # report coverage
 make cover_ci
 

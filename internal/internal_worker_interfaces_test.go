fa03a746181176dbf4755c759a1424f6ea128506
Shreyas Srivatsan <shreyass@uber.com>
2019-06-18 14:36:02 -0700

    fix test

--- internal/internal_worker_interfaces_test.go
+++ internal/internal_worker_interfaces_test.go
@@ -174,6 +174,6 @@ func (s *InterfacesTestSuite) TestInterface() {
 		DecisionTaskStartToCloseTimeout: 10 * time.Second,
 	}
 	workflowClient := NewClient(s.service, domain, nil)
-	wfExecution, err := workflowClient.StartWorkflow(context.Background(), workflowOptions, "workflowType")
+	_, err := workflowClient.StartWorkflow(context.Background(), workflowOptions, "workflowType")
 	s.NoError(err)
 }

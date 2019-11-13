3d1e6f12636c1a8f991c59e637081c3ecd03a071
Yichao Yang <ycyang@uber.com>
2019-11-13 11:40:35 -0800

    check if ctx is nil

--- internal/internal_workflow_client.go
+++ internal/internal_workflow_client.go
@@ -673,12 +673,14 @@ func (wc *workflowClient) ListArchivedWorkflow(ctx context.Context, request *s.L
 			var err1 error
 			timeout := maxListArchivedWorkflowTimeout
 			now := time.Now()
-			if expiration, ok := ctx.Deadline(); ok && expiration.After(now) {
-				timeout = expiration.Sub(now)
-				if timeout > maxListArchivedWorkflowTimeout {
-					timeout = maxListArchivedWorkflowTimeout
-				} else if timeout < minRPCTimeout {
-					timeout = minRPCTimeout
+			if ctx != nil {
+				if expiration, ok := ctx.Deadline(); ok && expiration.After(now) {
+					timeout = expiration.Sub(now)
+					if timeout > maxListArchivedWorkflowTimeout {
+						timeout = maxListArchivedWorkflowTimeout
+					} else if timeout < minRPCTimeout {
+						timeout = minRPCTimeout
+					}
 				}
 			}
 			tchCtx, cancel, opt := newChannelContext(ctx, chanTimeout(timeout))

a049acf00f85fd11eb8fee20051571f336683826
Bowei Xu <boweixu@uber.com>
2019-11-25 18:34:13 -0800

    Fix deterministic check for searchable change version

---
+++ test/replay_tests/workflows.go
@@ -0,0 +1,92 @@
+package replay_tests
+
+import (
+	"context"
+	"time"
+
+	"go.uber.org/cadence/activity"
+	"go.uber.org/cadence/workflow"
+	"go.uber.org/zap"
+)
+
+// ApplicationName is the task list for this sample
+const ApplicationName = "helloWorldGroup"
+
+func init() {
+	workflow.Register(Workflow)
+	workflow.Register(Workflow2)
+	activity.Register(helloworldActivity)
+}
+
+// Workflow workflow decider
+func Workflow(ctx workflow.Context, name string) error {
+	ao := workflow.ActivityOptions{
+		ScheduleToStartTimeout: time.Minute,
+		StartToCloseTimeout:    time.Minute,
+		HeartbeatTimeout:       time.Second * 20,
+	}
+	ctx = workflow.WithActivityOptions(ctx, ao)
+
+	logger := workflow.GetLogger(ctx)
+	logger.Info("helloworld workflow started")
+	var helloworldResult string
+	v := workflow.GetVersion(ctx, "test-change", workflow.DefaultVersion, 1)
+	if v == workflow.DefaultVersion {
+		err := workflow.ExecuteActivity(ctx, helloworldActivity, name).Get(ctx, &helloworldResult)
+		if err != nil {
+			logger.Error("Activity failed.", zap.Error(err))
+			return err
+		}
+	} else {
+		err := workflow.ExecuteActivity(ctx, helloworldActivity, name).Get(ctx, &helloworldResult)
+		err = workflow.ExecuteActivity(ctx, helloworldActivity, name).Get(ctx, &helloworldResult)
+		if err != nil {
+			logger.Error("Activity failed.", zap.Error(err))
+			return err
+		}
+	}
+
+	err := workflow.ExecuteActivity(ctx, helloworldActivity, name).Get(ctx, &helloworldResult)
+	if err != nil {
+		logger.Error("Activity failed.", zap.Error(err))
+		return err
+	}
+
+	logger.Info("Workflow completed.", zap.String("Result", helloworldResult))
+
+	return nil
+}
+
+// Workflow2 workflow decider
+func Workflow2(ctx workflow.Context, name string) error {
+	ao := workflow.ActivityOptions{
+		ScheduleToStartTimeout: time.Minute,
+		StartToCloseTimeout:    time.Minute,
+		HeartbeatTimeout:       time.Second * 20,
+	}
+	ctx = workflow.WithActivityOptions(ctx, ao)
+
+	logger := workflow.GetLogger(ctx)
+	logger.Info("helloworld workflow started")
+	var helloworldResult string
+
+	workflow.GetVersion(ctx, "test-change", workflow.DefaultVersion, 1)
+
+	workflow.UpsertSearchAttributes(ctx, map[string]interface{}{"CustomKeywordField": "testkey"})
+
+	err := workflow.ExecuteActivity(ctx, helloworldActivity, name).Get(ctx, &helloworldResult)
+	if err != nil {
+		logger.Error("Activity failed.", zap.Error(err))
+		return err
+	}
+
+	logger.Info("Workflow completed.", zap.String("Result", helloworldResult))
+
+	return nil
+}
+
+func helloworldActivity(ctx context.Context, name string) (string, error) {
+	logger := activity.GetLogger(ctx)
+	logger.Info("helloworld activity started")
+	return "Hello " + name + "!", nil
+}

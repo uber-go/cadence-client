1290c55f0759917a235b2b7a1729ab5bc0d1f90b
Liang Mei <meiliang@uber.com>
2020-04-24 09:57:16 -0700

    Update license

--- worker/worker.go
+++ worker/worker.go
@@ -1,4 +1,6 @@
-// Copyright (c) 2017 Uber Technologies, Inc.
+// Copyright (c) 2017-2020 Uber Technologies, Inc.
+//
+// Copyright (c) 2020 Temporal Technologies Inc.  All rights reserved.
 //
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the "Software"), to deal

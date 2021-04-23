519813c0ca935fd2c4cfa1a0068bb273fdfa2ffc
Vytautas Karpavicius <vytautas@uber.com>
2021-04-23 16:40:18 +0300

    Update yarpc to the latest version

--- .gen/go/cadence/workflowserviceclient/client.go
+++ .gen/go/cadence/workflowserviceclient/client.go
@@ -24,14 +24,14 @@
 package workflowserviceclient
 
 import (
-	"context"
-	"go.uber.org/cadence/.gen/go/cadence"
-	"go.uber.org/cadence/.gen/go/shared"
-	"go.uber.org/thriftrw/wire"
-	"go.uber.org/yarpc"
-	"go.uber.org/yarpc/api/transport"
-	"go.uber.org/yarpc/encoding/thrift"
-	"reflect"
+	context "context"
+	cadence "go.uber.org/cadence/.gen/go/cadence"
+	shared "go.uber.org/cadence/.gen/go/shared"
+	wire "go.uber.org/thriftrw/wire"
+	yarpc "go.uber.org/yarpc"
+	transport "go.uber.org/yarpc/api/transport"
+	thrift "go.uber.org/yarpc/encoding/thrift"
+	reflect "reflect"
 )
 
 // Interface is a client for the WorkflowService service.

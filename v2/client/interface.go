322d4d1bb118fb01bb6df63ba5d352db06f7363f
vytautas <vytautas@uber.com>
2021-10-05 12:12:19 +0000

    Move everything to interface

--- v2/client/interface.go
+++ v2/client/interface.go
@@ -54,14 +54,17 @@ type (
 		Get(name string) Domain
 	}
 
-	// ListedDomain is a domain that was retrievied via Domains.List function.
-	// It also contains info for the domain that was included in the List API call.
+	// ListedDomain is a domain that was retrieved via Domains.List function.
+	// It contains additional info for the domain.
 	ListedDomain interface {
 		Domain
 		Info() cadence.DomainInfo
 	}
 
+	// DescribedDomain is a domain that was retrieved via Domain.Describe function.
+	// It contains additional info for the domain.
 	DescribedDomain interface {
+		Domain
 		Info() cadence.DomainInfo
 	}
 
@@ -119,12 +122,17 @@ type (
 		GetCurrent(workflowID string) Workflow
 	}
 
+	// ListedWorkflow is a workflow retrieved via Workflows.List function.
+	// It contains additional info about the workflow that was included in the List API call.
 	ListedWorkflow interface {
 		Workflow
 		Info() cadence.WorkflowInfo
 	}
 
+	// DescribedWorkflow is a workflow retrieved via Workflow.Describe function.
+	// It contains additional info about the workflow.
 	DescribedWorkflow interface {
+		Workflow
 		Info() cadence.WorkflowInfo
 
 		// PendingActivities returns information about pending activities for the workflow (if any)
@@ -228,3 +236,320 @@ type (
 		List(ctx context.Context) ([]cadence.SearchAttribute, error)
 	}
 )
+
+// Page contains pagination data
+// Zero value Page{}, means first page with default size
+type Page struct {
+	Size  int32  // 0 - means default page size
+	Token []byte // nil - means first page
+}
+
+// FirstPage returns zero value Page struct which can be used as first page with default page size
+func FirstPage() Page {
+	return Page{}
+}
+
+type (
+	// DomainReplicationConfig configures domain replication. Use GlobalDomain or LocalDomain to create it.
+	DomainReplicationConfig interface {
+		domainReplicationConfig()
+	}
+
+	// DomainRegisterOption allows passing optional parameters when registering a domain
+	DomainRegisterOption interface {
+		domainRegisterOption()
+	}
+
+	// DomainUpdateOption allows passing optional parameters when updating the domain
+	DomainUpdateOption interface {
+		domainUpdateOption()
+	}
+
+	// DomainListOption allows passing optional parameters when listing domains
+	DomainListOption interface {
+		domainListOption()
+	}
+
+	// DomainDescribeOption allows passing optional parameters when describing domain
+	DomainDescribeOption interface {
+		domainDescribeOption()
+	}
+
+	// DomainFailoverOption allows passing optional parameters when making a domain failover
+	DomainFailoverOption interface {
+		domainFailoverOption()
+	}
+
+	// DomainDeprecateOption allows passing optional parameters when deprecating a domain
+	DomainDeprecateOption interface {
+		domainDeprecateOption()
+	}
+
+	// BadBinaryListOption allows passing optional parameters when listing bad binaries
+	BadBinaryListOption interface {
+		badBinaryListOption()
+	}
+
+	// BadBinaryAddOption allows passing optional parameters when adding bad binaries
+	BadBinaryAddOption interface {
+		badBinaryAddOption()
+	}
+
+	// BadBinaryDeleteOption allows passing optional parameters when deleting bad binaries
+	BadBinaryDeleteOption interface {
+		badBinaryDeleteOption()
+	}
+
+	// DomainOption can be used when registering or updating a domain
+	DomainOption interface {
+		DomainRegisterOption
+		DomainUpdateOption
+	}
+
+	// WorkflowStartOption allows passing optional parameters when starting a workflow.
+	WorkflowStartOption interface {
+		workflowStartOption()
+	}
+
+	// WorkflowSignalOption allows passing optional parameters when signaling a workflow.
+	WorkflowSignalOption interface {
+		workflowSignalOption()
+	}
+
+	// WorkflowQueryOption allows passing optional parameters when querying a workflow.
+	WorkflowQueryOption interface {
+		workflowQueryOption()
+	}
+
+	// WorkflowDescribeOption allows passing optional parameters when describing a workflow.
+	WorkflowDescribeOption interface {
+		workflowDescribeOption()
+	}
+
+	// WorkflowCancelOption allows passing optional parameters when canceling a workflow.
+	WorkflowCancelOption interface {
+		workflowCancelOption()
+	}
+
+	// WorkflowTerminateOption allows passing optional parameters when terminating a workflow.
+	WorkflowTerminateOption interface {
+		workflowTerminateOption()
+	}
+
+	// WorkflowResetOption allows passing optional parameters when resetting a workflow.
+	WorkflowResetOption interface {
+		workflowResetOption()
+	}
+
+	// WorkflowGetResultOption allows passing optional parameters when retrieving workflow result.
+	WorkflowGetResultOption interface {
+		workflowGetResultOption()
+	}
+
+	// WorkflowCountOption allows passing optional parameters when counting workflows.
+	WorkflowCountOption interface {
+		workflowCountOption()
+	}
+
+	// WorkflowListOption allows passing optional parameters when listing workflows.
+	WorkflowListOption interface {
+		workflowListOption()
+	}
+
+	// ActivityCompleteOption allows passing optional parameters when asynchronously completing an activity.
+	ActivityCompleteOption interface {
+		activityCompleteOption()
+	}
+
+	// ActivityHeartbeatOption allows passing optional parameters when heartbeating an activity.
+	ActivityHeartbeatOption interface {
+		activityHeartbeatOption()
+	}
+
+	// WorkflowResetPoint specifies the point in history for workflow reset.
+	WorkflowResetPoint interface {
+		workflowResetPoint()
+	}
+)
+
+// GlobalDomain can be used when registering a new domain to create its replication configuration.
+// - clusters specify in which clusters domain will be registered
+// - activeCluster specify an inital active cluster for the domain. This can later be changed with domain failover operation.
+func GlobalDomain(activeCluster string, clusters ...string) DomainReplicationConfig {
+	panic("not implemented")
+}
+
+// LocalDomain can be used when registering a new domain to create its replication configuration.
+// LocalDomain will register domain only in one specified cluster and will not setup any replication.
+func LocalDomain(cluster string) DomainReplicationConfig {
+	panic("not implemented")
+}
+
+// SetDomainDescription will set description of the domain
+func SetDomainDescription(description string) DomainOption {
+	panic("not implemented")
+}
+
+// SetDomainOwnerEmail will set owner email of the domain
+func SetDomainOwnerEmail(email string) DomainOption {
+	panic("not implemented")
+}
+
+// SetWorkflowRetentionPeriod will set workflow execution retention period of the domain
+func SetWorkflowRetentionPeriod(period time.Duration) DomainOption {
+	panic("not implemented")
+}
+
+// SetDomainData will set arbitrary data map provided by the user for the domain
+func SetDomainData(data map[string]string) DomainOption {
+	panic("not implemented")
+}
+
+// SetHistoryArchival will set history archival parameters for the domain
+func SetHistoryArchival(status cadence.ArchivalStatus, uri string) DomainOption {
+	panic("not implemented")
+}
+
+// SetVisibilityArchival will set visibility archival parameters for the domain
+func SetVisibilityArchival(status cadence.ArchivalStatus, uri string) DomainOption {
+	panic("not implemented")
+}
+
+// WithGracefulFailover will use a graceful domain failover with provided timeout
+func WithGracefulFailover(timeout time.Duration) DomainFailoverOption {
+	panic("not implemented")
+}
+
+// WithWorkflowID sets the business identifier of the workflow execution.
+// Default: generated UUID
+func WithWorkflowID(workflowID string) WorkflowStartOption {
+	panic("not implemented")
+}
+
+// WithDecisionTaskStartToCloseTimeout sets the timeout for processing decision task from the time the worker
+// pulled this task. If a decision task is lost, it is retried after this timeout.
+// The resolution is seconds.
+// Default: 10 seconds
+func WithDecisionTaskStartToCloseTimeout(timeout time.Duration) WorkflowStartOption {
+	panic("not implemented")
+}
+
+// WithWorkflowIDReusePolicy defines whether server allow reuse of workflow ID.
+// Can be useful for dedup logic if set to WorkflowIdReusePolicyRejectDuplicate.
+// Default: WorkflowIDReusePolicyAllowDuplicateFailedOnly
+func WithWorkflowIDReusePolicy(policy cadence.WorkflowIDReusePolicy) WorkflowStartOption {
+	panic("not implemented")
+}
+
+// WithRetryPolicy sets the retry policy for the workflow.
+// If provided, in case of workflow failure server will start new workflow execution if needed based on the retry policy.
+func WithRetryPolicy(policy cadence.RetryPolicy) WorkflowStartOption {
+	panic("not implemented")
+}
+
+// WithCronSchedule sets the cron schedule for workflow. If a cron schedule is specified, the workflow will run
+// as a cron based on the schedule. The scheduling will be based on UTC time. Schedule for next run only happen
+// after the current run is completed/failed/timeout. If a RetryPolicy is also supplied, and the workflow failed
+// or timeout, the workflow will be retried based on the retry policy. While the workflow is retrying, it won't
+// schedule its next run. If next schedule is due while workflow is running (or retrying), then it will skip that
+// schedule. Cron workflow will not stop until it is terminated or cancelled (by returning cadence.CanceledError).
+// The cron spec is as following:
+// ┌───────────── minute (0 - 59)
+// │ ┌───────────── hour (0 - 23)
+// │ │ ┌───────────── day of the month (1 - 31)
+// │ │ │ ┌───────────── month (1 - 12)
+// │ │ │ │ ┌───────────── day of the week (0 - 6) (Sunday to Saturday)
+// │ │ │ │ │
+// │ │ │ │ │
+// * * * * *
+func WithCronSchedule(cron string) WorkflowStartOption {
+	panic("not implemented")
+}
+
+// WithMemo sets non-indexed info that will be shown in list workflow.
+func WithMemo(memo map[string]interface{}) WorkflowStartOption {
+	panic("not implemented")
+}
+
+// WithSearchAttributes sets indexed info that can be used in query of List/Count workflow APIs (only
+// supported when Cadence server is using ElasticSearch). The key and value type must be registered on Cadence server side.
+// Use client.SearchAttributes().List() to get valid keys and corresponding value types.
+func WithSearchAttributes(searchAttributes map[string]interface{}) WorkflowStartOption {
+	panic("not implemented")
+}
+
+// WithDelayedStart sets the delay of the workflow start.
+// The resolution is seconds.
+// Default: 0 seconds
+func WithDelayedStart(delay time.Duration) WorkflowStartOption {
+	panic("not implemented")
+}
+
+// WithStart will start a workflow if he workflow is not running or not found, it starts the workflow and then sends the signal in transaction.
+func WithStart(wfFunc interface{}, args []interface{}, taskList string, timeout time.Duration, opts ...WorkflowStartOption) WorkflowSignalOption {
+	panic("not implemented")
+}
+
+// WithQueryRejectCondition sets the query behaviour based on workflow state.
+func WithQueryRejectCondition(condition cadence.QueryRejectCondition) WorkflowQueryOption {
+	panic("not implemented")
+}
+
+// WithQueryConsistencyLevel sets the consistency level on query.
+// Default: QueryConsistencyLevelEventual
+func WithQueryConsistencyLevel(level cadence.QueryConsistencyLevel) WorkflowQueryOption {
+	panic("not implemented")
+}
+
+// WithTerminateReason provides a details why workflows is being terminated.
+func WithTerminateDetails(details []byte) WorkflowTerminateOption {
+	panic("not implemented")
+}
+
+// WithNoSignalReapply will not apply signals after the reset point.
+func WithNoSignalReapply() WorkflowResetOption {
+	panic("not implemented")
+}
+
+// ResetToLastCompletedDecision will reset the workfow to the last completed decision.
+func ResetToLastCompletedDecision() WorkflowResetPoint {
+	panic("not implemented")
+}
+
+// ResetToLastScheduledDecision will reset the workflow to the last scheduled decision.
+func ResetToLastScheduledDecision() WorkflowResetPoint {
+	panic("not implemented")
+}
+
+// ResetToLastContinueAsNew will reset the workflow to the last continue-as-new.
+func ResetToLastContinueAsNew() WorkflowResetPoint {
+	panic("not implemented")
+}
+
+// ResetToFirstCompletedDecision will reset the workflow to the first completed decision.
+func ResetToFirstCompletedDecision() WorkflowResetPoint {
+	panic("not implemented")
+}
+
+// ResetToFirstScheduledDecision will reset the workflow to the first scheduled decision.
+func ResetToFirstScheduledDecision() WorkflowResetPoint {
+	panic("not implemented")
+}
+
+// ResetToBadBinary will reset the workflow to the given bad binary checksum.
+func ResetToBadBinary(checksum string) WorkflowResetPoint {
+	panic("not implemented")
+}
+
+// ResetToEarliestDecisionCompletedAfter will reset the workflow to the earliest completed decition after the given timestamp.
+func ResetToEarliestDecisionCompletedAfter(timestamp time.Time) WorkflowResetPoint {
+	panic("not implemented")
+}
+
+// ResetToEventId will reset the workflow to exact given event ID.
+func ResetToEventId(eventId int64) WorkflowResetPoint {
+	panic("not implemented")
+}
+
+type Query interface {
+}

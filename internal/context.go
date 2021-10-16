6e90393122d2fa843bab609f9c975beed3c573ce
demirkayaender <80807029+demirkayaender@users.noreply.github.com>
2021-10-15 19:19:47 -0700

    Fix parent vs child race (#1141)

--- internal/context.go
+++ internal/context.go
@@ -223,14 +223,19 @@ func propagateCancel(parent Context, child canceler) {
 		return // parent is never canceled
 	}
 	if p, ok := parentCancelCtx(parent); ok {
+		p.cancelLock.Lock()
 		if p.err != nil {
+			p.cancelLock.Unlock()
 			// parent has already been canceled
 			child.cancel(false, p.err)
 		} else {
+			p.childrenLock.Lock()
 			if p.children == nil {
 				p.children = make(map[canceler]bool)
 			}
 			p.children[child] = true
+			p.childrenLock.Unlock()
+			p.cancelLock.Unlock()
 		}
 	} else {
 		go func() {
@@ -269,6 +274,9 @@ func removeChild(parent Context, child canceler) {
 	if !ok {
 		return
 	}
+
+	p.childrenLock.Lock()
+	defer p.childrenLock.Unlock()
 	if p.children != nil {
 		delete(p.children, child)
 	}
@@ -288,11 +296,12 @@ type cancelCtx struct {
 
 	done Channel // closed by the first cancel call.
 
-	mu       sync.Mutex
-	canceled bool
+	cancelLock sync.Mutex
+	canceled   bool
 
-	children map[canceler]bool // set to nil by the first cancel call
-	err      error             // set to non-nil by the first cancel call
+	childrenLock sync.Mutex
+	children     map[canceler]bool // set to nil by the first cancel call
+	err          error             // set to non-nil by the first cancel call
 }
 
 func (c *cancelCtx) Done() Channel {
@@ -307,32 +316,48 @@ func (c *cancelCtx) String() string {
 	return fmt.Sprintf("%v.WithCancel", c.Context)
 }
 
+func (c *cancelCtx) getChildren() []canceler {
+	c.childrenLock.Lock()
+	defer c.childrenLock.Unlock()
+
+	out := []canceler{}
+	for key := range c.children {
+		out = append(out, key)
+	}
+	return out
+}
+
 // cancel closes c.done, cancels each of c's children, and, if
 // removeFromParent is true, removes c from its parent's children.
 func (c *cancelCtx) cancel(removeFromParent bool, err error) {
-	c.mu.Lock()
+	c.cancelLock.Lock()
 	if c.canceled {
-		c.mu.Unlock()
+		c.cancelLock.Unlock()
 		// calling cancel from multiple go routines isn't safe
 		// avoid a data race by only allowing the first call
 		return
 	}
 	c.canceled = true
-	c.mu.Unlock()
 
 	if err == nil {
 		panic("context: internal error: missing cancel error")
 	}
 	if c.err != nil {
+		c.cancelLock.Unlock()
 		return // already canceled
 	}
 	c.err = err
+	c.cancelLock.Unlock()
 	c.done.Close()
-	for child := range c.children {
+
+	children := c.getChildren()
+	for _, child := range children {
 		// NOTE: acquiring the child's lock while holding parent's lock.
 		child.cancel(false, err)
 	}
+	c.childrenLock.Lock()
 	c.children = nil
+	c.childrenLock.Unlock()
 
 	if removeFromParent {
 		removeChild(c.Context, c)

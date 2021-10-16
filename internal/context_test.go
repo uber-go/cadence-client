7044e1d8a3d2f193e2648590bdb2c4cd7f06eb95
demirkayaender <80807029+demirkayaender@users.noreply.github.com>
2021-10-15 19:19:47 -0700

    Fix parent vs child race (#1141)

--- internal/context_test.go
+++ internal/context_test.go
@@ -29,7 +29,49 @@ import (
 	"go.uber.org/zap/zaptest"
 )
 
-func TestContext_RaceRegression(t *testing.T) {
+func TestContextChildParentCancelRace(t *testing.T) {
+	/*
+		Testing previous race happened while child and parent cancelling at the same time
+		While child is trying to remove itself from the parent, parent tries to iterate
+		its children and cancel them at the same time.
+	*/
+	env := newTestWorkflowEnv(t)
+
+	wf := func(ctx Context) error {
+		parentCtx, parentCancel := WithCancel(ctx)
+		defer parentCancel()
+
+		type cancelerContext struct {
+			ctx      Context
+			canceler func()
+		}
+
+		children := []cancelerContext{}
+		numChildren := 100
+
+		for i := 0; i < numChildren; i++ {
+			c, canceler := WithCancel(parentCtx)
+			children = append(children, cancelerContext{
+				ctx:      c,
+				canceler: canceler,
+			})
+		}
+
+		for i := 0; i < numChildren; i++ {
+			go children[i].canceler()
+			if i == numChildren/2 {
+				go parentCancel()
+			}
+		}
+
+		return nil
+	}
+	env.RegisterWorkflow(wf)
+	env.ExecuteWorkflow(wf)
+	assert.NoError(t, env.GetWorkflowError())
+}
+
+func TestContextConcurrentCancelRace(t *testing.T) {
 	/*
 		A race condition existed due to concurrently ending goroutines on shutdown (i.e. closing their chan without waiting
 		on them to finish shutdown), which executed... quite a lot of non-concurrency-safe code in a concurrent way.  All
@@ -46,6 +88,35 @@ func TestContext_RaceRegression(t *testing.T) {
 		ctx, cancel := WithCancel(ctx)
 		racyCancel := func(ctx Context) {
 			defer cancel() // defer is necessary as Sleep will never return due to Goexit
+			_ = Sleep(ctx, time.Hour)
+		}
+		// start a handful to increase odds of a race being detected
+		for i := 0; i < 10; i++ {
+			Go(ctx, racyCancel)
+		}
+
+		_ = Sleep(ctx, time.Minute) // die early
+		return nil
+	}
+	env.RegisterWorkflow(wf)
+	env.ExecuteWorkflow(wf)
+	assert.NoError(t, env.GetWorkflowError())
+}
+
+func TestContextAddChildCancelParentRace(t *testing.T) {
+	/*
+		It's apparently also possible to race on adding children while propagating the cancel to children.
+	*/
+	env := newTestWorkflowEnv(t)
+	wf := func(ctx Context) error {
+		ctx, cancel := WithCancel(ctx)
+		racyCancel := func(ctx Context) {
+			defer cancel() // defer is necessary as Sleep will never return due to Goexit
+			defer func() {
+				_, ccancel := WithCancel(ctx)
+				cancel()
+				ccancel()
+			}()
 			_ = Sleep(ctx, time.Hour)
 		}
 		// start a handful to increase odds of a race being detected

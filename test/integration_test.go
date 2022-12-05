66b0c9ab7db9271d31189a1c9e3b43959db32526
Mindaugas Barcauskas <mindaugas.barcauskas@uber.com>
2022-12-05 09:43:55 +0200

    addressing review comments.

--- test/integration_test.go
+++ test/integration_test.go
@@ -419,11 +419,9 @@ func (ts *IntegrationTestSuite) TestChildWFWithParentClosePolicyTerminate() {
 	var childWorkflowID string
 	err := ts.executeWorkflow("test-childwf-parent-close-policy", ts.workflows.ChildWorkflowSuccessWithParentClosePolicyTerminate, &childWorkflowID)
 	ts.NoError(err)
-	resp, err := ts.libClient.DescribeWorkflowExecution(context.Background(), childWorkflowID, "")
-	ts.NoError(err)
 	// Need to wait for child workflow to finish as well otherwise test becomes flaky
-	ts.waitForWorkflowFinish(resp.WorkflowExecutionInfo.Execution.GetWorkflowId(), resp.WorkflowExecutionInfo.Execution.GetRunId())
-	resp, err = ts.libClient.DescribeWorkflowExecution(context.Background(), childWorkflowID, "")
+	ts.waitForWorkflowFinish(childWorkflowID, "")
+	resp, err := ts.libClient.DescribeWorkflowExecution(context.Background(), childWorkflowID, "")
 	ts.NoError(err)
 	ts.True(resp.WorkflowExecutionInfo.GetCloseTime() > 0)
 
@@ -590,8 +588,7 @@ func (ts *IntegrationTestSuite) waitForWorkflowFinish(wid string, runId string) 
 	ctx, cancel := context.WithTimeout(context.Background(), ctxTimeout)
 	defer cancel()
 	wfRun := ts.libClient.GetWorkflow(ctx, wid, runId)
-	var val interface{}
-	return wfRun.Get(ctx, &val)
+	return wfRun.Get(ctx, nil)
 }
 
 var _ interceptors.WorkflowInterceptorFactory = (*tracingInterceptorFactory)(nil)

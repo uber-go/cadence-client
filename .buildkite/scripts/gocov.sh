b9d4298906708266095aed33ba9c5d2308f1eab7
Steven L <imgroxx@gmail.com>
2023-02-03 21:08:48 -0600

    try to fix coverage

--- .buildkite/scripts/gocov.sh
+++ .buildkite/scripts/gocov.sh
@@ -7,10 +7,10 @@ go get github.com/mattn/goveralls
 
 # download cover files from all the tests
 mkdir -p .build/coverage
-buildkite-agent artifact download ".build/coverage/unit_test_cover.out" . --step ":golang: unit-test" --build "$BUILDKITE_BUILD_ID"
-buildkite-agent artifact download ".build/coverage/integ_test_sticky_off_cover.out" . --step ":golang: integration-test-sticky-off" --build "$BUILDKITE_BUILD_ID"
-buildkite-agent artifact download ".build/coverage/integ_test_sticky_on_cover.out" . --step ":golang: integration-test-sticky-on" --build "$BUILDKITE_BUILD_ID"
-buildkite-agent artifact download ".build/coverage/integ_test_grpc_cover.out" . --step ":golang: integration-test-grpc-adapter" --build "$BUILDKITE_BUILD_ID"
+buildkite-agent artifact download ".build/*/coverage/unit_test_cover.out" . --step ":golang: unit-test" --build "$BUILDKITE_BUILD_ID"
+buildkite-agent artifact download ".build/*/coverage/integ_test_sticky_off_cover.out" . --step ":golang: integration-test-sticky-off" --build "$BUILDKITE_BUILD_ID"
+buildkite-agent artifact download ".build/*/coverage/integ_test_sticky_on_cover.out" . --step ":golang: integration-test-sticky-on" --build "$BUILDKITE_BUILD_ID"
+buildkite-agent artifact download ".build/*/coverage/integ_test_grpc_cover.out" . --step ":golang: integration-test-grpc-adapter" --build "$BUILDKITE_BUILD_ID"
 
 echo "download complete"
 

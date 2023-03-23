6270570cd45bac4cf584b5cbd902d0e86a0f880e
agautam <agautam@uber.com>
2023-03-23 13:00:55 -0700

    Added the test for coroutine where removal of branch should have caused failure

--- test/replaytests/parallel_workflow.go
+++ test/replaytests/parallel_workflow.go
@@ -66,6 +66,53 @@ func sampleParallelWorkflow(ctx workflow.Context) error {
 	return nil
 }
 
+// Removing one branch from coroutine 1 made no difference. Here we remove one branch ie branch1.2from the coroutine 1. `
+func sampleParallelWorkflow2(ctx workflow.Context) error {
+	waitChannel := workflow.NewChannel(ctx)
+
+	ao := workflow.ActivityOptions{
+		ScheduleToStartTimeout: time.Minute,
+		StartToCloseTimeout:    time.Minute,
+		HeartbeatTimeout:       time.Second * 20,
+	}
+	ctx = workflow.WithActivityOptions(ctx, ao)
+
+	logger := workflow.GetLogger(ctx)
+	workflow.Go(ctx, func(ctx workflow.Context) {
+		err := workflow.ExecuteActivity(ctx, sampleActivity, "branch1.1").Get(ctx, nil)
+		if err != nil {
+			logger.Error("Activity failed", zap.Error(err))
+			waitChannel.Send(ctx, err.Error())
+			return
+		}
+		waitChannel.Send(ctx, "")
+	})
+
+	workflow.Go(ctx, func(ctx workflow.Context) {
+		err := workflow.ExecuteActivity(ctx, sampleActivity, "branch2").Get(ctx, nil)
+		if err != nil {
+			logger.Error("Activity failed", zap.Error(err))
+			waitChannel.Send(ctx, err.Error())
+			return
+		}
+		waitChannel.Send(ctx, "")
+	})
+
+	// wait for both of the coroutinue to complete.
+	var errMsg string
+	for i := 0; i != 2; i++ {
+		waitChannel.Receive(ctx, &errMsg)
+		if errMsg != "" {
+			err := errors.New(errMsg)
+			logger.Error("Coroutine failed", zap.Error(err))
+			return err
+		}
+	}
+
+	logger.Info("Workflow completed.")
+	return nil
+}
+
 func sampleActivity(input string) (string, error) {
 	name := "sampleActivity"
 	fmt.Printf("Run %s with input %v \n", name, input)

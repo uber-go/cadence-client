0e269f23d70a508aaf9e720e4aa08c0f7ec923e5
agautam <agautam@uber.com>
2023-03-23 14:49:06 -0700

    Added copyright info

--- test/replaytests/branch_workflow.go
+++ test/replaytests/branch_workflow.go
@@ -1,3 +1,23 @@
+// Copyright (c) 2017 Uber Technologies, Inc.
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
 package replaytests
 
 import (
@@ -40,7 +60,7 @@ func sampleBranchWorkflow(ctx workflow.Context) error {
 }
 
 // SampleBranchWorkflow2 run a workflow with different number of branch.
-//If the number of expected branches is changed the replayer should catch it as a non deterministic error.
+// If the number of expected branches is changed the replayer should catch it as a non deterministic error.
 func sampleBranchWorkflow2(ctx workflow.Context) error {
 	var futures []workflow.Future
 	// starts activities in parallel

60a08321283bbf2236e57fa7745912c7973d834b
agautam <agautam@uber.com>
2023-03-28 10:23:32 -0700

    Fixed merge issues

--- test/replaytests/branch_workflow.go
+++ test/replaytests/branch_workflow.go
@@ -88,10 +88,3 @@ func sampleBranchWorkflow2(ctx workflow.Context) error {
 
 	return nil
 }
-
-func sampleActivity(input string) (string, error) {
-	name := "sampleActivity"
-	fmt.Printf("Run %s with input %v \n", name, input)
-
-	return "Result_" + name, nil
-}

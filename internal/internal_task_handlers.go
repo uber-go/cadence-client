4dd2716c89bd00c7feebc6891668159650517073
Jakob Haahr Taankvist <jht@uber.com>
2023-09-12 11:12:54 +0200

    Only log failed heartbeats if the error is not CanceledError (#1271)
    * Lowered the log level to info as this floods logs when canceling workflows
    
    * Changed from lowering the error level to just checking if it's a canceled error

--- internal/internal_task_handlers.go
+++ internal/internal_task_handlers.go
@@ -1496,7 +1496,10 @@ func (i *cadenceInvoker) heartbeatAndScheduleNextRun(details []byte) error {
 
 			// Log the error outside the lock.
 			if err != nil {
-				i.logger.Error("Failed to send heartbeat", zap.Error(err), zap.String(tagWorkflowType, i.workflowType), zap.String(tagActivityType, i.activityType))
+				// If the error is a canceled error do not log, as this is expected.
+				if _, ok := err.(*CanceledError); !ok {
+					i.logger.Error("Failed to send heartbeat", zap.Error(err), zap.String(tagWorkflowType, i.workflowType), zap.String(tagActivityType, i.activityType))
+				}
 			}
 		}()
 	}

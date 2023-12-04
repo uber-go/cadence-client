2ac07688bdc5e4e6abfe350b470405cdcf7ea90d
Tim Li <ltim@uber.com>
2023-12-04 14:00:57 -0800

    change logging to debug level and add more details to prevent confusion

--- internal/internal_utils.go
+++ internal/internal_utils.go
@@ -468,6 +468,8 @@ func estimateHistorySize(logger *zap.Logger, event *s.HistoryEvent) int {
 			sum += len(event.SignalExternalWorkflowExecutionInitiatedEventAttributes.Control)
 			sum += len(event.SignalExternalWorkflowExecutionInitiatedEventAttributes.Input)
 		}
+	default:
+		logger.Debug("unsupported event type for history size estimation", zap.String("Event Type", event.GetEventType().String()))
 	}
 
 	return sum

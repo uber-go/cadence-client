1f84cfb16ffb69ea1143c22fa8f6da91ef3ced2d
Steven L <imgroxx@gmail.com>
2024-04-01 15:11:50 -0500

    Update golint.sh
    going back to before, really not sure any more how bash / sh / $() work.
    so match the server setup so we at least have the same behavior (which is reasonably well tested both there and here), and fixing both will be easier if needed.

--- scripts/golint.sh
+++ scripts/golint.sh
@@ -1,18 +1,14 @@
-#!/usr/bin/env sh
+#!/bin/sh
 
-set -exu
-
-#make tidy
-#make build
-
-val="$(git status --porcelain 2>&1)"
-
-echo 'still here'
+# refresh deps
+make tidy
+# regenerate, format, and make sure everything builds
+make build
 
 # intentionally capture stderr, so status-errors are also PR-failing.
 # in particular this catches "dubious ownership" failures, which otherwise
 # do not fail this check and the $() hides the exit code.
-if [[ -n $(git status --porcelain 2>&1) ]]; then
+if [ -n "$(git status --porcelain  2>&1)" ]; then
   echo "There file changes after applying your diff and performing a build."
   echo "Please run this command and commit the changes:"
   echo "\tmake tidy && make build"

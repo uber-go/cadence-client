7650eb58a10fcf8530674fe98bad9f4b7d43e962
Steven L <imgroxx@gmail.com>
2024-04-01 12:36:13 -0500

    try bash and [[

--- scripts/golint.sh
+++ scripts/golint.sh
@@ -1,14 +1,14 @@
-#!/bin/sh
+#!/usr/bin/env bash
 
 set -exu -o pipefail
 
-make tidy
-make build
+#make tidy
+#make build
 
 # intentionally capture stderr, so status-errors are also PR-failing.
 # in particular this catches "dubious ownership" failures, which otherwise
 # do not fail this check and the $() hides the exit code.
-if [ -n "$(git status --porcelain  2>&1)" ]; then
+if [[ -n $(git status --porcelain 2>&1) ]]; then
   echo "There file changes after applying your diff and performing a build."
   echo "Please run this command and commit the changes:"
   echo "\tmake tidy && make build"

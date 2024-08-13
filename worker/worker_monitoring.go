b214c09f18cc36cd104c3e17182fa1c249817599
Ketsia <ketsia.mbaku@uber.com>
2024-08-13 14:07:39 +0200

    fix typo

--- worker/worker_monitoring.go
+++ worker/worker_monitoring.go
@@ -25,7 +25,8 @@ import "go.uber.org/cadence/internal"
 type (
 	// EventsMonitoring provides a set of methods that can be used to collect
 	// stats on the Worker for debugging purposes.
-	EventsMonitoring = internal.EventMonitoring
+	// Deprecated: in development and very likely to change
+	EventMonitoring = internal.EventMonitoring
 
 	// PollerLifeCycle contains a set of methods to collect information on running pollers for a worker
 	// Deprecated: in development and very likely to change
